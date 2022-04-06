dc62434ad664014f17f804b19a7c8ff5017cc42f
rishi12438 <rishimahadevan@gmail.com>
2022-04-06 21:22:24 +0800

    added styling and support

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ const btnValues = [
 ];
 
 const toLocaleString = (num) =>
-  String(num).replace(/(?<!\..*)(\d)(?=(?:\d{3})+(?:\.|$))/g, "$1 ");
+  String(num).replace(/(\d)(?=(?:\d{3})+(?:\.|$))/g, "$1 ");
 
 const removeSpaces = (num) => num.toString().replace(/\s/g, "");
 
